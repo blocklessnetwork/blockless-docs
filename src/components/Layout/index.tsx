6b44d207131842c300c1154c4bc65037f1c93de6
gaoxin <490299928@qq.com>
2023-05-04 11:05:41 +0800

    remove debug

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -18,7 +18,6 @@ const Layout: React.FC<PropsTypes> = (props) => {
 	const { pageMap } = pageOpts
 	const { locale = DEFAULT_LOCALE, defaultLocale } = useRouter()
 	const fsPath = useFSRoute()
-  console.log("pageMap",JSON.stringify(pageMap))
 	const { activeType, docsDirectories, directories } = useMemo(
 		() =>
 			normalizePages({
