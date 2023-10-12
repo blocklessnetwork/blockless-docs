671e085250268b443cbffcd3c263408c55501b41
Liam Zhang <imliamzhang@gmail.com>
2023-10-11 18:15:22 -0700

    fixing key issue on home page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -45,7 +45,7 @@ const Home: FC = () => {
 									viewBox="0 0 15 15"
 									fill="none"
 									xmlns="http://www.w3.org/2000/svg"
-                  className="nx-text-mutedForeground nx-w-[15px] group-hover:nx-w-[20px] nx-transition-all"
+									className="nx-text-mutedForeground nx-w-[15px] group-hover:nx-w-[20px] nx-transition-all"
 								>
 									<path
 										d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
@@ -60,7 +60,7 @@ const Home: FC = () => {
 						<p className="nx-mt-3 nx-text-mutedForeground nx-text-sm/[1.7em]">{item.description}</p>
 						<div className="nx-mt-3 nx-flex nx-flex-col nx-gap-3">
 							{item.links.map((nav, k) => (
-								<Link href={nav.link}>
+								<Link href={nav.link} key={k}>
 									<p
 										key={k}
 										className="md:nx-text-mutedForeground nx-text-foreground  nx-text-sm/[1.7em] md:nx-text-left nx-text-center nx-font-medium md:nx-underline nx-no-underline nx-underline-offset-[3px] nx-border md:nx-border-none nx-border-border nx-py-2 md:nx-py-0 nx-rounded-md md:nx-rounded-none hover:nx-text-slate-900"
